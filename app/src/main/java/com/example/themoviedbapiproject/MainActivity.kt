8536fec44a25a0db95698fc18b671794ff67b449
HatidzheOnbashieva <hatidzhe_onbashieva@abv.bg>
2021-12-06 14:15:22 +0200

    "sign in & sign up screen"

--- app/src/main/java/com/example/themoviedbapiproject/MainActivity.kt
+++ app/src/main/java/com/example/themoviedbapiproject/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.themoviedbapiproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.themoviedbapiproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+//        if (supportFragmentManager.fragments.isEmpty()) {
+//
+//        }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, SignInFragment()).commit()
+
     }
 }
